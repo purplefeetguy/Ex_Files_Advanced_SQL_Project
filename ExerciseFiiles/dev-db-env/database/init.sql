d373c210be3770eca6d3aad45782c74eb3984e3b
purplefeetguy@hotmail.com <purplefeetguy@hotmail.com>
2024-12-29 08:43:36 -1000

    inital attempt issues with properly creating and persisting the db

---
+++ ExerciseFiiles/dev-db-env/database/init.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE example_table (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name TEXT
+-- );
+
+CREATE TABLE example_table (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+INSERT INTO example_table (name) VALUES ('Test UserName');
+INSERT INTO example_table (name) VALUES ('Mark Silva');
+INSERT INTO example_table (name) VALUES ('Hector Kamara');
+INSERT INTO example_table (name) VALUES ('Greg Anthony');
